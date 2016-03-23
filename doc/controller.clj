aa2e56daa3aeeba65c25c2f8255453b2ced4af48
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-03-23 09:28:15 +0100

    config: use ports 6380 (TCP SSL RPC) & 6381 (HTTP API)

--- doc/controller.clj
+++ doc/controller.clj
@@ -1,7 +1,7 @@
-{:transport {:port 1337 :ssl {:ca-cert "/etc/warp/certs/ca-crt.pem"
-                              :cert    "/etc/warp/certs/server.pem"
-                              :pkey    "/etc/warp/certs/server.key."}}
- :api {:port 8593
-       :origins ["http://127.0.0.1:*" "http://localhost:*"]}
- :scenarios "site/scenarios"
+{:transport {:port 6380
+             :ssl  {:ca-cert "/etc/warp/certs/ca-crt.pem"
+                    :cert    "/etc/warp/certs/server.pem"
+                    :pkey    "/etc/warp/certs/server.pkey64.p8"}}
+ :api       {:port 6381}
+ :scenarios "/etc/warp/scenarios"
  :keepalive 30}
